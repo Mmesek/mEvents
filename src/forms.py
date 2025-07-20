d484a89d64dfd91793c6fa53a2a114267dfcbb4d
Mmesek <13630781+Mmesek@users.noreply.github.com>
2025-07-21 00:51:13 +0200

    WIP updates to match db schema
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- src/forms.py
+++ src/forms.py
@@ -1,23 +1,37 @@
 from dataclasses import dataclass
 
 from components import FormSectionDiv
+from generators import QuestionType
 
 
 @dataclass
 class Question:
     id: int = None
-    question: str = None
+    title: str = None
     type: str = None
     kwargs: dict = None
+    max_length: int = None
+    min_length: int = None
     description: str = None
+    allow_multiple_answer: bool = None
+    Question_Options: dict = None
+
+    def __post_init__(self):
+        self.type = QuestionType.get(self.type)
 
     def generate(self):
         return FormSectionDiv(
             self.type(
-                question=self.question,
+                question=self.title,
                 question_id=self.id,
                 description=self.description,
-                **(self.kwargs or {}),
+                **{
+                    "max_length": self.max_length,
+                    "min_length": self.min_length,
+                    "min": self.min_length,
+                    "max": self.max_length,
+                    "options": [i["value"] for i in self.Question_Options],
+                },
             )
         )
 
