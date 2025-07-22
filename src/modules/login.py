275d57731dcb1cb92506d760346a485b8bce2e94
Mmesek <13630781+Mmesek@users.noreply.github.com>
2025-07-23 00:45:50 +0200

    Add email OTP & Google login
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- src/modules/login.py
+++ src/modules/login.py
@@ -1,13 +1,32 @@
 import os
 
-from fasthtml.common import A, Redirect, fast_app
-from monsterui.all import Card, Titled, Theme
+from fasthtml.common import A, Redirect, fast_app, P
+from monsterui.all import (
+    Card,
+    Titled,
+    Theme,
+    Form,
+    Input,
+    Button,
+    ButtonT,
+)
 
 from db import supa
 
 BASE_URL = os.getenv("BASE_URL", "http://localhost:5001")
 
 app, rt = fast_app(hdrs=Theme.orange.headers())
+
+LOGIN_STRING = "Zaloguj korzystając z {}"
+
+
+def magic_link():
+    return Form(
+        LOGIN_STRING.format("magicznego linku"),
+        Input(id="email", placeholder="email@hostname.tld"),
+        Button("Wyślij link do logowania", cls=ButtonT.primary),
+        hx_post="/login/email",
+    )
 
 
 @rt("/")
@@ -16,18 +35,26 @@ def login():
         "Login",
         Card(
             A(
-                "Login with Discord",
+                LOGIN_STRING.format("Discorda"),
                 href="https://discord.com",
                 hx_get="/login/discord",
             ),
         ),
         Card(
             A(
-                "Login with Facebook",
+                LOGIN_STRING.format("Google'a"),
+                href="https://google.com",
+                hx_get="/login/google",
+            )
+        ),
+        Card(
+            A(
+                LOGIN_STRING.format("Facebooka"),
                 href="https://facebook.com",
                 hx_get="/login/facebook",
             ),
         ),
+        Card(magic_link()),
     )
 
 
@@ -51,11 +78,58 @@ def login_facebook():
     return oauth_login("facebook")
 
 
+@rt("/google")
+def login_google():
+    return oauth_login("google")
+
+
+@rt("/email")
+def login_email(email: str, session):
+    try:
+        res = supa.auth.sign_in_with_otp(
+            {
+                "email": email,
+                "options": {"email_redirect_to": f"{BASE_URL}/login/verify"},
+            }
+        )
+    except:
+        return "Wprowadzony adres e-mail jest nie poprawny", magic_link()
+    session["otp_email"] = email
+    return P(
+        "Sprawdź swojego maila oraz kliknij w link bądź wpisz tutaj kod z maila aby się zalogować:"
+    ), Form(
+        Input(id="otp", placeholder="123456"),
+        Button("Użyj kodu", cls=ButtonT.primary),
+        hx_post="/login/otp",
+    )
+
+
+@rt("/otp")
+def otp(otp: str, session):
+    res = supa.auth.verify_otp(
+        {"email": session["otp_email"], "token": otp, "type": "email"}
+    )
+    session["email"] = res.user.email
+    session["id"] = res.user.id
+    session["picture"] = f"https://api.dicebear.com/8.x/lorelei/svg?seed={res.user.id}"
+    return Redirect("/")
+
+
 @rt("/redirect")
 def redirect(code: str, session):
     res = supa.auth.exchange_code_for_session({"auth_code": code})
     session["email"] = res.user.email
     session["id"] = res.user.id
     session["picture"] = res.user.user_metadata.get("avatar_url")
+
+    return Redirect("/")
+
+
+@rt("/verify")
+def verify_otp(access_token: str, type: str, session):
+    res = supa.auth.verify_otp({"token_hash": access_token, "type": type})
+    session["email"] = res.user.email
+    session["id"] = res.user.id
+    session["picture"] = f"https://api.dicebear.com/8.x/lorelei/svg?seed={res.user.id}"
 
     return Redirect("/")
