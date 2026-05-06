09258ba635f3fa0c73517533afd5c7260c5eb752
Mmesek <13630781+Mmesek@users.noreply.github.com>
2026-05-06 21:19:03 +0200

    Fix invalid refresh_token by reauthing
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- src/beforeware.py
+++ src/beforeware.py
@@ -2,7 +2,7 @@ import time
 from fasthtml import common as fh
 from src.components.translations import Translation
 from src.db import supa
-from supabase_auth import AuthResponse
+from supabase_auth import AuthResponse, errors
 
 
 def store_session(res: AuthResponse, session: dict):
@@ -23,9 +23,14 @@ def user_auth_before(req, sess):
         return fh.RedirectResponse("/login", 303)
 
 
-def refresh_session(sess):
+def refresh_session(req, sess):
     if sess.get("refresh_token") and sess.get("expires_at", 0) < time.time():
-        store_session(supa.auth.refresh_session(sess.get("refresh_token")), sess)
+        try:
+            auth = supa.auth.refresh_session(sess.get("refresh_token"))
+            store_session(auth, sess)
+        except errors.AuthApiError:
+            sess.pop("refresh_token")
+            return user_auth_before(req, sess)
 
 
 def set_locale(sess):
