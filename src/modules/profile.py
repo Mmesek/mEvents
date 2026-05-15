ef0b01f3494f4b6f098a1db65ee174672ace9e04
Mmesek <13630781+Mmesek@users.noreply.github.com>
2026-05-15 02:43:26 +0200

    Add profile settings & buttons
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- src/modules/profile.py
+++ src/modules/profile.py
@@ -13,6 +13,34 @@ class Profile(Base):
     user_id: str
     display_name: str
 
+    def edit(self, session):
+        return mui.DivCentered(
+            mui.Form(
+                mui.Card(
+                    mui.LabelInput(
+                        "Imię",
+                        placeholder="Imię N",
+                        value=self.display_name,
+                        title="Nazwa pod którą można cię rozpoznać",
+                    ),
+                    mui.LabelInput(
+                        "Podłączony e-mail",
+                        placeholder="email@example.com",
+                        value=session["email"],
+                        title="E-mail do kontaktu",
+                        disabled=True,
+                    ),
+                    mui.Switch(
+                        "Zgoda na przetwarzanie wizerunku",
+                        title="W celu publikacji na mediach społecznościowych",
+                        disabled=True,
+                    ),
+                    mui.Button("Zapisz", disabled=True),
+                    mui.DividerSplit("Połączone konta"),
+                )
+            )
+        )
+
     def render(self, session):
         last_event = Attendance.get_one(
             Attendance.select(session["auth"], 'arrived, left, event:"Event" (title)')
@@ -36,20 +64,36 @@ class Profile(Base):
                         ),
                     ),
                     mui.DividerSplit("EXP"),
-                    mui.DivCentered(mui.Legend("20/100"), mui.Progress(title="20/100", value=20)),
+                    mui.DivCentered(mui.Legend("0/100"), mui.Progress(title="0/100", value=0)),
                     header=mui.DivFullySpaced(
                         self.display_name,
                         fh.Img(src=session.get("picture"), height="64", width="64"),
                     ),
                 ),
-                mui.Button("Ustawienia"),
+                fh.A(mui.Button("Ustawienia"), href="/profile/settings"),
                 mui.DivCentered(
-                    mui.Grid(
-                        mui.Button("Zadania"),
-                        mui.Button("Poziomy"),
-                        mui.Button("Wydarzenia"),
-                        mui.Button("Feedback"),
-                        cols=4,
+                    mui.Card(
+                        mui.DividerSplit("Przejdź do"),
+                        mui.Grid(
+                            mui.Button(fh.A("Wydarzeń", href="/events")),
+                            mui.Button(fh.A("Feedbacku", href="/feedback"), disabled=True),
+                            cols=2,
+                        ),
+                        mui.DividerSplit("Zarządzaj"),
+                        mui.Grid(
+                            mui.Button(fh.A("Zadania", href="/quests"), disabled=True),
+                            mui.Button(fh.A("Poziomy", href="/profile/levels"), disabled=True),
+                            mui.Button(fh.A("Postać", href="/characters"), disabled=True),
+                            mui.Button(fh.A("Ekwipunek", href="/items"), disabled=True),
+                            cols=4,
+                        ),
+                        mui.DividerSplit("Moje..."),
+                        mui.Grid(
+                            mui.Button(fh.A("Wydarzenia", href="/events/mine")),
+                            mui.Button(fh.A("Deklaracje", href="/contributions"), disabled=True),
+                            mui.Button(fh.A("Bilety", href="/tickets"), disabled=True),
+                            cols=3,
+                        ),
                     ),
                 ),
             ),
@@ -60,4 +104,11 @@ class Profile(Base):
 @with_layout()
 def profile(session):
     # p = Profile.get_one(Profile.select(session["auth"]))
-    return Profile(session["id"], "Mmesek").render(session)
+    return Profile(session["id"], session["display_name"]).render(session)
+
+
+@rt
+@with_layout()
+def settings(session):
+    # p = Profile.get_one(Profile.select(session["auth"]))
+    return Profile(session["id"], session["display_name"]).edit(session)
