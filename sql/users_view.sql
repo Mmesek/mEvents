f0a45cf62213b0f42bf341b4ab8cbc6464db0f5d
Mmesek <13630781+Mmesek@users.noreply.github.com>
2026-06-29 00:00:34 +0200

    Update users to use Profile data
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- sql/users_view.sql
+++ sql/users_view.sql
@@ -3,16 +3,22 @@ OR REPLACE VIEW forms.users AS
 SELECT
   DISTINCT u.id AS id,
   COALESCE(
-    u.raw_user_meta_data ->> 'full_name',
-    split_part(u.raw_user_meta_data ->> 'email', '@', 1)
+    p.display_name,
+    COALESCE(
+      u.raw_user_meta_data ->> 'full_name',
+      split_part(u.raw_user_meta_data ->> 'email', '@', 1)
+    )
   ) AS display_name,
-  u.email,
   r.event_id,
+  COALESCE(p.email, u.email) AS email,
   MAX(r."timestamp") AS "timestamp"
 FROM
   auth.users u
-  INNER JOIN forms."Response" r ON u.id = r.user_id
+  INNER JOIN forms."Response" r ON u.id = r.user_id FULL
+  OUTER JOIN forms."Profile" p ON u.id = p.user_id
 GROUP BY
   u.id,
   r.event_id,
+  p.display_name,
+  p.email,
   u.raw_user_meta_data;
